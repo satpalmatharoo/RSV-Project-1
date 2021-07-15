2a0bee8e6d664ba3c6913685de60f7b0d8358454
FJVIKTORIJA <83015268+FJVIKTORIJA@users.noreply.github.com>
2021-07-15 21:34:38 +0100

    Sharing in class teamwork progress

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var yourForm = document.getElementById("search-form");
+var searchInput = document.getElementById("search-input");
+
+yourForm.addEventListener("submit", function (event) {
+    event.defaultPrevented();
+
+    var searchPhrase = searchInput.value;
+
+    var previousSearches = JSON.parse(localStorage.getItem("searches"));
+    previousSearches.push(searchPhrase);
+
+    localStorage.setItem("seaches", JSON.stringify(previousSearches));
+})
+
+
+
+
+// localStorage.setItem("satpal", "sunny");
+
+
+// var satpal = localStorage.getItem("satpal") // sunny
+
+// var students = [{name: "Viktoria"}];
+
+// localStorage.setItem("students", JSON.stringify(students))
+
+
+// var studentsFromLocalStorage = JSON.parse(localStorage.getItem("students"));
+
+// console.log("The name of the first student in the array is: " + studentsFromLocalStorage[0].name)
