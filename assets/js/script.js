88029ed7b145b256609810159c621eae2f022d1a
FJVIKTORIJA <83015268+FJVIKTORIJA@users.noreply.github.com>
2021-07-20 18:47:25 +0100

    Groupcall/ APIs added
    Added APIs to script.js. Local storage implemented.
    
    Co-Authored-By: satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
    Co-Authored-By: Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,58 @@
+var API_KEY = "6f42252eb8msh67e41ddac877ed0p1b148bjsn0467aff798de";
+
+function getquote() {
+  fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "6f42252eb8msh67e41ddac877ed0p1b148bjsn0467aff798de",
+      "x-rapidapi-host": "quotes15.p.rapidapi.com",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      document.getElementById("text").innerHTML = response.content;
+      document.getElementById("creator").innerHTML =
+        "- " + response.originator.name + " -";
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+// wikipediaapi
+
 var searchForm = document.getElementById("search-form");
 var searchInput = document.getElementById("search-input");
 
-searchForm.addEventListener("submit", function (event) {
+searchForm.addEventListener("click", function (event) {
   event.preventDefault();
 
   var searchPhrase = searchInput.value;
-  execute(searchPhrase);
-
-  //   var previousSearches = JSON.parse(localStorage.getItem("searches"));
-  //   previousSearches.push(searchPhrase);
-
-  //   localStorage.setItem("seaches", JSON.stringify(previousSearches));
-});
-
-function loadClient() {
-  gapi.client.setApiKey("YAIzaSyC4kDwWv8x8zYaJ0sF-ijugwtjp3S1wlcQ");
-  return gapi.client
-    .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-    .then(
-      function () {
-        console.log("AIzaSyC4kDwWv8x8zYaJ0sF-ijugwtjp3S1wlcQ");
+  console.log(searchPhrase);
+  fetch(
+    "https://wiki-scraper.p.rapidapi.com/?article=" +
+      searchPhrase +
+      "&par=4&lan=en",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "6f42252eb8msh67e41ddac877ed0p1b148bjsn0467aff798de",
+        "x-rapidapi-host": "wiki-scraper.p.rapidapi.com",
       },
-      function (err) {
-        console.error("Error loading GAPI client for API", err);
-      }
-    );
-}
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function execute(search) {
-  return gapi.client.youtube.search
-    .list({
-      part: ["snippet"],
-      maxResults: 25,
-      q: search,
+    }
+  )
+    .then((r) => r.text())
+    .then((response) => {
+      console.log(response);
+      localStorage.setItem("search", searchPhrase);
+      localStorage.setItem("search-results", response);
+
+      document.getElementById("search").innerHTML = searchPhrase;
+      document.getElementById("search-results").innerHTML = response;
     })
-    .then(
-      function (response) {
-        // Handle the results here (response.result has the parsed body).
-        console.log("Response", response);
-      },
-      function (err) {
-        console.error("Execute error", err);
-      }
-    );
-}
-
-function handleClientLoad() {
-  gapi.load("client:auth2", function () {
-    gapi.auth2.init({
-      client_id:
-        "624001377429-hjju337cp0o6rgcatmoiq3dr6429f8kq.apps.googleusercontent.com",
+    .catch((err) => {
+      console.error(err);
     });
-  });
-}
-
-// localStorage.setItem("satpal", "sunny");
-
-// var satpal = localStorage.getItem("satpal") // sunny
-
-// var students = [{name: "Viktoria"}];
-
-// localStorage.setItem("students", JSON.stringify(students))
-
-// var studentsFromLocalStorage = JSON.parse(localStorage.getItem("students"));
-
-// console.log("The name of the first student in the array is: " + studentsFromLocalStorage[0].name)
+});
+document.getElementById("search").innerHTML = localStorage.getItem("search");
+document.getElementById("search-results").innerHTML =
+  localStorage.getItem("search-results");
